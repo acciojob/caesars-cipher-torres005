43980b8ece8011cde9aa51edf676afa0be6c5791
torres005 <109883423+torres005@users.noreply.github.com>
2023-05-12 18:49:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,8 +13,16 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	for(let i =0;i<encodedStr.length;i++){
+		var sciNum = encodedStr[i].charCodeAt();
+		if(sciNum>= 65 && sciNum<=77){
+			decodedArr += String.fromCharCode(sciNum + 13);
+		}else if(sciNum>=78 && sciNum<=90){
+			decodedArr += String.fromCharCode(sciNum - 13);
+		}
+	}
 
-  return ;//return decodedArr
+  return decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
